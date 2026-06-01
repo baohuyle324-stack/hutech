c538a8a16495fffeb2e65beef9014176143130f8
baohuyle324 <baohuyle324@gmail.com>
2026-06-01 10:16:20 +0700

    Loại bỏ Lab-01 khỏi nhánh lab-02

--- Lab-01/ex02/ex02_10.py
+++
@@ -1,5 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-#Su dung ham dao_nguoc_chuoi
-input_string = input("Mời nhập chuỗi cần đảo ngược: ")
-print(f"Chuỗi sau khi đảo ngược là: {dao_nguoc_chuoi(input_string)}")
